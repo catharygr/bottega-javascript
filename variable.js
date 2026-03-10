2d947fde756648bc961acc6646c5e855f9236d57
catharygr <103951447+catharygr@users.noreply.github.com>
2026-03-10 10:30:52 +0100

    Iniciando con Js

--- variable.js
+++ variable.js
@@ -1,3 +1,33 @@
-const YoSoy = [2, 3, 4, 5];
+// var names, city, age;
+// names = "Kristine";
+// city = "Scottsdale";
+// age = 12;
 
-console.log(YoSoy);
+// console.log(age);
+// console.log(names);
+// console.log(city);
+
+// var
+// var age = 12;
+// console.log(age);
+
+// var age = 15; // yep!
+// console.log(age);
+
+// let
+// let age = 12;
+// console.log(age);
+
+// let age = 15; // nope!
+// console.log(age);
+
+// Hoisted
+// names = 'Kristine';
+// console.log(names); // 'Kristine'
+// var name;
+
+// // Initializers not hoisted
+// console.log(age); // undefined
+// var age = 33;
+
+/
