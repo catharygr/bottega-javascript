a6df145e8fcfcbb1a5533d4996aeae1f9b131518
catharygr <103951447+catharygr@users.noreply.github.com>
2026-03-12 18:48:20 +0100

    Lecciones de JS

---
+++ array.js
@@ -0,0 +1,11 @@
+const arr = [0, 1, 2, 3, 4, 5];
+
+function quitarEleemntos(arg) {
+  arg.pop();
+  arg.shift();
+  return arr;
+}
+console.log(quitarEleemntos(arr));
+
+// Create an array that has 3 elements, there must be a least one number within the array.
+var myArray = ["Hi", 1, { name: "Caty" }];
