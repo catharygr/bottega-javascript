8e2189b439448cc065bac5cb2400e5c65b1f4452
catharygr <103951447+catharygr@users.noreply.github.com>
2026-03-25 16:28:26 +0100

    Trabajando en npm

---
+++ moment.js
@@ -0,0 +1,8 @@
+import moment from "moment/moment";
+
+const rightNow = moment();
+
+// const birhtday = moment("1983-07-31", "YYYY-MM-DD");
+console.log(birthday.format("dddd"));
+const birthday = moment("2018-01-12", "YYYY-MM-DD");
+console.log(birthday.format("MMM Do YYYY"));
